6df38d0578cce1db57a4cde7caae1310160ab14d
EmilioAK <emilio.alvarez96@gmail.com>
2020-06-23 20:16:24 +0200

    Make numbers and signs populate outputbox

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ const operations = {
 };
 
 const operate = (operation, n1, n2) => operations[operation](n1, n2);
+
+const numberButtons = document.querySelectorAll(".numberButton");
+const operationButtons = document.querySelectorAll('.operationButton');
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear');
+
+const outputBox = document.querySelector('#outputBox');
+
+
+const populateOutputBox = function (buttonType) {
+    buttonType.forEach((button) => {
+        button.addEventListener('click', () => {
+            elementToDisplay = document.createTextNode(button.value);
+            outputBox.appendChild(elementToDisplay);
+        })
+    })
+}
+
+clearButton.addEventListener('click', () => {
+    outputBox.innerHTML = '';
+})
+
+equalsButton.addEventListener('click', () => {
+})
+
+populateOutputBox(numberButtons);
+populateOutputBox(operationButtons);
